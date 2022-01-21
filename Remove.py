b51f2abedda73e2ee469b5ffde2ea9a7ee86f58c
NetAWSM <netroad2ge@mail.ru>
2022-01-21 17:25:57 +0300

    first commit

---
+++ Remove.py
@@ -0,0 +1,20 @@
+import os
+
+
+path = "C:\Python\ect"
+ex = ".txt"
+
+if os.path.exists(path):
+  if os.path.isdir(path):
+    for root_folder, folders, files in os.walk(path):
+      for file in files:
+        file_path = os.path.join(root_folder, file)
+        file_extention = os.path.splitext(file_path)[1]
+        if ex == file_extention:
+          os.remove(file_path)
+
+            
+
+
+        
+
