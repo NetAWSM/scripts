6501b48095ed889e05d6cb3f26f04f8c88a55672
NetAWSM <netroad2ge@mail.ru>
2022-01-22 03:31:24 +0300

    Relise

--- Remove.py
+++ Remove.py
@@ -1,17 +1,31 @@
 import os
 
 
-path = "C:\\Users\\irina.zarubina\\Desktop\\ed"
+path = []
 ex = ".pdf"
 
-if os.path.exists(path):
-  if os.path.isdir(path):
-    for root_folder, folders, files in os.walk(path):
-      for file in files:
-        file_path = os.path.join(root_folder, file)
-        file_extention = os.path.splitext(file_path)[1]
-        if ex == file_extention:
-          os.remove(file_path)
+
+for root, dirs, files in os.walk("C:\\users"):
+  if '\send mail' in root:
+      aa = root
+      path.append(aa)
+
+try:
+  for i in path:
+    if os.path.exists(i):
+      if os.path.isdir(i):
+        for root_folder, folders, files in os.walk(i):
+          for file in files:
+            file_path = os.path.join(root_folder, file)
+            file_extention = os.path.splitext(file_path)[1]
+            if ex == file_extention:
+              os.remove(file_path)
+
+    else:
+      print('Такой дириктории нет')
+
+except:
+  print('Такой дириктории нет')
 
             
 
